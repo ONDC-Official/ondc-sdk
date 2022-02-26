6dad67513cb2d7db70e0f0e52b62315e81c04b3b
navdeep710 <navdeep710@gmail.com>
2022-02-26 12:30:34 +0530

    Added new endpoint and ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -67,7 +67,7 @@ esac
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
 docker-compose run --rm --entrypoint "\
-  certbot certonly --webroot -w /var/www/certbot \
+  certbot certonly --webroot -v -w /var/www/certbot \
     $staging_arg \
     $email_arg \
     $domain_args \
